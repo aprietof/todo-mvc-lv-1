5cfe4d4d31615b627fcd37e17a5f9113519c24b5
aprietof <adrianprieto00@gmail.com>
2016-08-05 12:04:34 -0400

    Item #complete? and #incomplete? methods and show view checkbox added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ActiveRecord::Base
   validates_presence_of :description
   belongs_to :list
+
+  STATUS = {
+    :complete => 1,
+    :incomplete => 0
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
+
 end
